8273efa77d3f1fed4f2f5b2a5c93232d52d10779
joaobragaciandt <joaobraga@ciandt.com>
2022-07-19 16:11:34 -0300

    1. MEI

---
+++ 0x01/mei/src/Person.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private Date birthDate;
+    private boolean anotherCompanyOwner;
+    private boolean pensioner;
+    private boolean publicServer;
+    private float salary;
+
+    public Person(String name, String surname, Date birthDate, boolean anotherCompanyOwner, boolean pensioner, boolean publicServer) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.anotherCompanyOwner = anotherCompanyOwner;
+        this.pensioner = pensioner;
+        this.publicServer = publicServer;
+    }
+
+    public String fullName() {
+        return name + " " + surname;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+        this.anotherCompanyOwner = anotherCompanyOwner;
+    }
+
+    public void setPensioner(boolean pensioner) {
+        this.pensioner = pensioner;
+    }
+
+    public void setPublicServer(boolean publicServer) {
+        this.publicServer = publicServer;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public float calculateYearlySalary() {
+        return getSalary() * 12;
+    }
+
+    public boolean isMEI() {
+        if(calculateYearlySalary() < 130000 && birthDate.getYear() >= 18 && !anotherCompanyOwner && !pensioner && !publicServer) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ 0x01/mei/src/PersonTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class PersonTest {
+
+    Person person;
+
+    @BeforeEach
+    public void setup() {
+        Date nascimento = new Date(2000);
+        person = new Person("Paul","McCartney",nascimento,true,true,true);
+    }
+
+    @Test
+    public void show_full_name() {
+        Assertions.assertEquals("Paul McCartney", person.fullName());
+    }
+
+    @Test
+    public void test_calculateYearlySalary() {
+        person.setSalary(1200);
+        Assertions.assertEquals(14400, person.calculateYearlySalary());
+    }
+
+    @Test
+    public void person_is_MEI() {
+        person.setPensioner(false);
+        person.setAnotherCompanyOwner(false);
+        person.setPublicServer(false);
+        person.setSalary(1200);
+        Assertions.assertTrue(person.isMEI());
+    }
+
+    @Test
+    public void person_is_not_MEI() {
+        Assertions.assertFalse(person.isMEI());
+    }
+
+
+}
